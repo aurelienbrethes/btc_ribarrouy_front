a3e1761c4393050713f2a3719e7c654fe1990d1a
aurelienbrethes <aurelienbrethes@gmail.com>
2022-02-21 11:15:01 +0100

    update css modal inscription

--- src/components/Events/ModalInscription.jsx
+++ src/components/Events/ModalInscription.jsx
@@ -98,102 +98,127 @@ const ModalInscription = ({
   };
 
   return (
-    <div className="modal" onClick={(e) => handleChildClick(e)}>
+    <div className="modalInscription" onClick={(e) => handleChildClick(e)}>
       <section>
-        <aside>
+        <aside className="modalInscription__left">
           <h2>{title}</h2>
           <p>{date}</p>
           <p>{place}</p>
           <p>{description}</p>
         </aside>
-        <aside>
+        <aside className="modalInscription__right">
           <form action="submit">
             <h2>Inscriptions :</h2>
-            <label htmlFor="lastname">Nom</label>
-            <input
-              type="text"
-              id="lastname"
-              onChange={(e) => setLastname(e.target.value)}
-            />
-            <label htmlFor="firstname">Prénom</label>
-            <input
-              type="text"
-              id="firstname"
-              onChange={(e) => setFirstname(e.target.value)}
-            />
-            <label htmlFor="club">Club</label>
-            <input
-              type="club"
-              id="club"
-              onChange={(e) => setClub(e.target.value)}
-            />
-            <label htmlFor="licenceNumber">Numéro de licence</label>
-            <input
-              type="text"
-              id="licenceNumber"
-              onChange={(e) => setLicence(e.target.value)}
-            />
-            <label htmlFor="phone">Téléphone</label>
-            <input
-              type="number"
-              id="phone"
-              onChange={(e) => setPhone(e.target.value)}
-            />
-            <label htmlFor="email">email</label>
-            <input
-              type="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="category">Catégorie</label>
-            <select
-              name="category"
-              id="category"
-              onChange={(e) => setCategory(e.value.target)}
-            >
-              <option value="1ereSerie">1ère Série</option>
-              <option value="2emeSerie">2ème Série</option>
-              <option value="3emeSerie">3ème Série</option>
-              <option value="4emeSerie">4ème Série</option>
-              <option value="veteran">Vétéran</option>
-              <option value="super veteran">Super Vétéran</option>
-              <option value="master">Master</option>
-              <option value="handisport">Handisport</option>
-              <option value="dame">Dame</option>
-              <option value="junior">Junior</option>
-              <option value="cadet">Cadet</option>
-            </select>
-            <label htmlFor="day">Jour</label>
-            <select
-              name="day"
-              id="day"
-              onChange={(e) => setDay(e.target.value)}
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-            </select>
-            <label htmlFor="food">Repas</label>
-            <select
-              name="food"
-              id="food"
-              onChange={(e) => setFood(e.target.value)}
-            >
-              {foodArray.map((item) => (
-                <option value={item}>{item}</option>
-              ))}
-            </select>
+            <div>
+              <label htmlFor="lastname">Nom</label>
+              <input
+                type="text"
+                id="lastname"
+                onChange={(e) => setLastname(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="firstname">Prénom</label>
+              <input
+                type="text"
+                id="firstname"
+                onChange={(e) => setFirstname(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="club">Club</label>
+              <input
+                type="club"
+                id="club"
+                onChange={(e) => setClub(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="licenceNumber">Numéro de licence</label>
+              <input
+                type="text"
+                id="licenceNumber"
+                onChange={(e) => setLicence(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="phone">Tél.</label>
+              <input
+                className="modalinscription__right-tel"
+                type="text"
+                id="phone"
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">email</label>
+              <input
+                type="email"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="category">Catégorie</label>
+              <select
+                name="category"
+                id="category"
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="1ère Serie">1ère Série</option>
+                <option value="2ème Serie">2ème Série</option>
+                <option value="3ème Serie">3ème Série</option>
+                <option value="4ème Serie">4ème Série</option>
+                <option value="vétéran">Vétéran</option>
+                <option value="super vétéran">Super Vétéran</option>
+                <option value="master">Master</option>
+                <option value="handisport">Handisport</option>
+                <option value="dame">Dame</option>
+                <option value="junior">Junior</option>
+                <option value="cadet">Cadet</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="day">Jour</label>
+              <select
+                name="day"
+                id="day"
+                onChange={(e) => setDay(e.target.value)}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="food">Repas</label>
+              <select
+                name="food"
+                id="food"
+                onChange={(e) => setFood(e.target.value)}
+              >
+                {foodArray.map((item, index) => (
+                  <option key={index} value={item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </div>
             <button type="submit" onClick={(e) => handleSubmit(e)}>
               S'inscrire
             </button>
           </form>
         </aside>
       </section>
-      <button onClick={() => setShowModal(false)}>Fermer</button>
-      {cookies.monCookie && (
-        <div>
-          <button onClick={() => handleChangeModal()}>Voir les inscrits</button>
-          <button onClick={() => handleUpdateEvent()}>Modifier</button>
-          <button onClick={() => setDeleteEvent(true)}>Supprimer</button>
+      {cookies.monCookie ? (
+        <div className="modalInscription__bottom">
+          <div>
+            <button onClick={() => handleChangeModal()}>
+              Voir les inscrits
+            </button>
+            <button onClick={() => handleUpdateEvent()}>Modifier</button>
+            <button onClick={() => setDeleteEvent(true)}>Supprimer</button>
+            <button onClick={() => setShowModal(false)}>Fermer</button>
+          </div>
           {deleteEvent && (
             <div>
               <p>Êtes-vous sûr de vouloir supprimer cet évènement ?</p>
@@ -202,6 +227,13 @@ const ModalInscription = ({
             </div>
           )}
         </div>
+      ) : (
+        <button
+          className="modalInscription__closeButton"
+          onClick={() => setShowModal(false)}
+        >
+          Fermer
+        </button>
       )}
     </div>
   );
